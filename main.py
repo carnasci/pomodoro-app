016ec8d5febbeb6b1151ccc02fbc17c8f7e0b149
carnasci <cen.master24@gmail.com>
2025-01-27 12:12:43 -0600

    Day 28 - added a few more UI widgets

--- main.py
+++ main.py
@@ -25,8 +25,23 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+timer_label = Label()
+timer_label.config(text="Timer", font=(FONT_NAME, 30, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+start_button = Button()
+start_button.config(text="Start", font=(FONT_NAME, 10, "bold"))
+start_button.grid(row=2, column=0, sticky="E")
+
+reset_button = Button()
+reset_button.config(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button.grid(row=2, column=2, sticky="W")
+
+checkmark_label = Label()
+checkmark_label.config(text="✓", font=(FONT_NAME, 16), fg=GREEN, bg=YELLOW)
+checkmark_label.grid(row=3, column=1)
 
 
 
